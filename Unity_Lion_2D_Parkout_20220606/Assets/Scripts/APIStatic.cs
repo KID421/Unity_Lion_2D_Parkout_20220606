8381114b5365c267fa88ea1b2e7103e9b1c47311
KID <50666207+KID421@users.noreply.github.com>
2022-06-20 10:07:30 +0800

    Update 練習 靜態 API

--- Unity_Lion_2D_Parkout_20220606/Assets/Scripts/APIStatic.cs
+++ Unity_Lion_2D_Parkout_20220606/Assets/Scripts/APIStatic.cs
@@ -7,6 +7,9 @@ namespace KID
     /// </summary>
     public class APIStatic : MonoBehaviour
     {
+        private Vector3 a = new Vector3(1, 1, 1);
+        private Vector3 b = new Vector3(22, 22, 22);
+
         private void Start()
         {
             #region 學習
@@ -31,7 +34,22 @@ namespace KID
             print("隨機範圍：" + range);
             #endregion
 
+            // 類別名稱.靜態屬性名稱
+            print("所有攝影機數量：" + Camera.allCamerasCount);
+            print("目前的平台：" + Application.platform);
 
+            Physics.sleepThreshold = 10;
+            print("睡眠臨界值：" + Physics.sleepThreshold);
+            Time.timeScale = 0.5f;
+            print("時間大小：" + Time.timeScale);
+
+            // 類別名稱.靜態方法名稱(對應的引數)
+            print("9.999 去掉小數點四捨五入：" + Mathf.Round(9.999f));
+
+            float distance = Vector3.Distance(a, b);
+            print("<color=yellow>距離：" + distance + "</color>");
+
+            Application.OpenURL("https://unity.com/");
         }
 
         private void Update()
@@ -41,7 +59,10 @@ namespace KID
             print("隨機整數 1 ~ 3：" + rangeInt);
             #endregion
 
+            // print("是否按下任意鍵：" + Input.anyKeyDown);
+            // print("遊戲經過時間：" + Time.timeSinceLevelLoad);
 
+            print("<color=red>是否按下空白鍵：" + Input.GetKeyDown(KeyCode.Space) + "</color>");
         }
     }
 }
